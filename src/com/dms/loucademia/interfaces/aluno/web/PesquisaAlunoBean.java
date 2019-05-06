cdcb587110481cdf8d0f313e70da7bc50ea74029
Diorgenes Morais <diorgenesmorais@gmail.com>
2019-05-06 07:11:29 -0300

    Limpando formulários Aula 9.2 Na verdade, limmpando as propriedades dos Beans envolvidos. Uma estrategia usada quando o Bean for diferente de RequestScoped.

--- src/com/dms/loucademia/interfaces/aluno/web/PesquisaAlunoBean.java
+++ src/com/dms/loucademia/interfaces/aluno/web/PesquisaAlunoBean.java
@@ -2,9 +2,11 @@ package com.dms.loucademia.interfaces.aluno.web;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.annotation.RequestParameterMap;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -31,6 +33,10 @@ public class PesquisaAlunoBean implements Serializable {
 	private Integer rg;
 	private Integer telefone;
 	private List<Aluno> alunos;
+
+	@Inject
+	@RequestParameterMap
+	private Map<String, String> requestParamsMap;
 
 	public String getMatricula() {
 		return matricula;
@@ -80,5 +86,17 @@ public class PesquisaAlunoBean implements Serializable {
 	public String excluir(String matricula) {
 		alunoService.delete(matricula);
 		return pesquisar();
+	}
+
+	public void check() {
+		String clear = requestParamsMap.get("clear");
+
+		if (clear != null && Boolean.valueOf(clear)) {
+			this.matricula = null;
+			this.nome = null;
+			this.rg = null;
+			this.telefone = null;
+			this.alunos = null;
+		}
 	}
 }
--- src/com/dms/loucademia/interfaces/relatorio/web/RelatorioSituacoesBean.java
+++ src/com/dms/loucademia/interfaces/relatorio/web/RelatorioSituacoesBean.java
@@ -2,9 +2,12 @@ package com.dms.loucademia.interfaces.relatorio.web;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.annotation.RequestParameterMap;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.dms.loucademia.application.service.AlunoService;
@@ -18,10 +21,14 @@ public class RelatorioSituacoesBean implements Serializable {
 	private static final long serialVersionUID = 9019308605548516137L;
 
 	private Situacao situacao;
-	
+
 	private List<Aluno> alunos;
 	@EJB
 	private AlunoService alunoService;
+
+	@Inject
+	@RequestParameterMap
+	private Map<String, String> requestParamsMap;
 
 	public Situacao getSituacao() {
 		return situacao;
@@ -39,5 +46,13 @@ public class RelatorioSituacoesBean implements Serializable {
 		this.alunos = alunoService.listSituacoesAlunos(situacao);
 		return null;
 	}
-	
+
+	public void check() {
+		String clear = requestParamsMap.get("clear");
+
+		if (clear != null && Boolean.valueOf(clear)) {
+			this.situacao = null;
+			this.alunos = null;
+		}
+	}
 }
